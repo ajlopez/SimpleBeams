0ac4c26f74bcafefe5cb9339b2e6809cafb19afe
ajlopez <webmaster@ajlopez.com>
2013-10-16 17:14:02 -0300

    Beam with filter

--- test/beam.js
+++ test/beam.js
@@ -8,3 +8,11 @@ exports['create beam and send simple message'] = function (test) {
     beam.send(1);
     test.equal(counter, 1);
 }
+
+exports['create beam with filter and send simple message'] = function (test) {
+    var counter = 0;
+    var beam = sb.createBeam(function (x) { return x > 0; }, function (x) { counter += x; });
+    test.ok(beam);
+    beam.send(1);
+    test.equal(counter, 1);
+}
