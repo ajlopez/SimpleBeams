517412a4420457c5a84f0fef15dd7b521e3b414e
ajlopez <webmaster@ajlopez.com>
2013-10-16 17:23:27 -0300

    Transform function has arity two

--- test/beam.js
+++ test/beam.js
@@ -3,7 +3,7 @@ var sb = require('..');
 
 exports['create beam and send simple message'] = function (test) {
     var counter = 0;
-    var beam = sb.createBeam(function (x) { counter += x; });
+    var beam = sb.createBeam(function (x, cb) { counter += x; });
     test.ok(beam);
     beam.send(1);
     test.equal(counter, 1);
@@ -11,17 +11,27 @@ exports['create beam and send simple message'] = function (test) {
 
 exports['create beam with filter and send simple message'] = function (test) {
     var counter = 0;
-    var beam = sb.createBeam(function (x) { return x > 0; }, function (x) { counter += x; });
+    var beam = sb.createBeam(function (x) { return x > 0; }, function (x, cb) { counter += x; });
     test.ok(beam);
     beam.send(1);
     test.equal(counter, 1);
 }
 
-
 exports['create beam with filter and reject simple message'] = function (test) {
     var counter = 0;
-    var beam = sb.createBeam(function (x) { return x % 2; }, function (x) { counter += x; });
+    var beam = sb.createBeam(function (x) { return x % 2; }, function (x, cb) { counter += x; });
     test.ok(beam);
     beam.send(2);
     test.equal(counter, 0);
+}
+
+exports['throw invalid arguments in none'] = function (test) {
+    test.throws(
+        function () {
+            sb.createBeam();
+        },
+        function (err) {
+            return err == 'invalid arguments';
+        }
+    );
 }
