ce775e4c9c2356a5b23289ebb8832a4d3343b23d
ajlopez <webmaster@ajlopez.com>
2013-10-17 07:32:04 -0300

    Accepts function with arity 1 as transformer

--- test/beam.js
+++ test/beam.js
@@ -3,7 +3,7 @@ var sb = require('..');
 
 exports['create beam and send simple message'] = function (test) {
     var counter = 0;
-    var beam = sb.createBeam(function (x, cb) { counter += x; });
+    var beam = sb.createBeam(function (x) { counter += x; });
     test.ok(beam);
     beam.send(1);
     test.equal(counter, 1);
@@ -11,7 +11,7 @@ exports['create beam and send simple message'] = function (test) {
 
 exports['create beam with filter and send simple message'] = function (test) {
     var counter = 0;
-    var beam = sb.createBeam(function (x) { return x > 0; }, function (x, cb) { counter += x; });
+    var beam = sb.createBeam(function (x) { return x > 0; }, function (x) { counter += x; });
     test.ok(beam);
     beam.send(1);
     test.equal(counter, 1);
@@ -19,7 +19,7 @@ exports['create beam with filter and send simple message'] = function (test) {
 
 exports['create beam with filter and reject simple message'] = function (test) {
     var counter = 0;
-    var beam = sb.createBeam(function (x) { return x % 2; }, function (x, cb) { counter += x; });
+    var beam = sb.createBeam(function (x) { return x % 2; }, function (x) { counter += x; });
     test.ok(beam);
     beam.send(2);
     test.equal(counter, 0);
