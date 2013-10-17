19a35e87d5821df19b8a3474410cd17934db99cd
ajlopez <webmaster@ajlopez.com>
2013-10-17 07:37:50 -0300

    Additional test, pipe two beams

--- test/pipe.js
+++ test/pipe.js
@@ -12,3 +12,16 @@ exports['pipe to beam'] = function (test) {
     
     test.equal(counter, 3);
 }
+
+exports['pipe to beams'] = function (test) {
+    var counter = 0;
+    
+    var beam = sb.createBeam(function (x) { return x > 0; });
+    beam.pipe(function (x) { counter += x });
+    beam.pipe(function (x) { counter *= x });
+    
+    beam.send(1);
+    beam.send(2);
+    
+    test.equal(counter, 6);
+}
